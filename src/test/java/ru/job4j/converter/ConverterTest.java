01acf2db5229831b87998184d90b36a70ae4678f
Konstantin <89169555954@mail.ru>
2024-10-14 23:46:49 +0300

    Фиксация изменений на github [#505270 #516742]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void rubleToDollar() {
+    }
+
+    @Test
+    void rubleToEuro() {
+    }
+
+    @Test
+    void dollarToRuble() {
+    }
+
+    @Test
+    void euroToRuble() {
+    }
+}
