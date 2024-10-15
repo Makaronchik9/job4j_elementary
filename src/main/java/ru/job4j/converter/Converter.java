b7dff3a95ad3d96f53ec5975d28a162f546a782a
Konstantin <89169555954@mail.ru>
2024-10-15 21:52:51 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,19 +1,4 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static double rubleToDollar(double value) {
-        return value / 60; // предположим, что 1 доллар = 60 рублей
-    }
-
-    public static double rubleToEuro(double value) {
-        return value / 70; // предположим, что 1 евро = 70 рублей
-    }
-
-    public static double dollarToRuble(double value) {
-        return value * 60;
-    }
-
-    public static double euroToRuble(double value) {
-        return value * 70;
-    }
 }
