a084f049fe78e8ef798d678bd9ce034de74254f5
Konstantin <89169555954@mail.ru>
2024-10-15 23:18:20 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/loop/LoopFOr.java
+++ src/main/java/ru/job4j/loop/LoopFOr.java
@@ -1,2 +1,9 @@
-package ru.job4j.loop;public class LoopFOr {
+package ru.job4j.loop;
+
+public class LoopFOr {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
