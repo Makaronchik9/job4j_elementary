61a42e3a9b6577370ad4be4fe469893ae6990b60
Konstantin <89169555954@mail.ru>
2024-10-17 00:39:49 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,8 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        // Создаем массив на 10 элементов типа short
         short[] ages = new short[10];
-
-        // Создаем массив на 100500 элементов типа String
         String[] surnames = new String[100500];
-
-        // Создаем массив на 40 элементов типа float
         float[] prices = new float[40];
 
     }
