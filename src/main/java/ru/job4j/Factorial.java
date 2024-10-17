760d8c94d1c7da87996df62beb21d04a1404fb4a
Konstantin <89169555954@mail.ru>
2024-10-17 23:04:57 +0300

    Фиксация изменений на github [#505270 #516742]

---
+++ src/main/java/ru/job4j/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+
+    public static int calculate(int n) {
+
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(Factorial.calculate(5));
+    }
+}
