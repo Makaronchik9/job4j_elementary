f461e13d4a4d5da05a2caad266c0e766c36db4f4
Konstantin <89169555954@mail.ru>
2024-10-17 23:54:47 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,33 +1,29 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
     public void whenSumFrom0To10Then55() {
         int result = Counter.sum(0, 10);
-        assertEquals(55, result); // сумма чисел от 0 до 10 должна быть 55
+        assertEquals(55, result);
     }
 
     @Test
     public void whenSumFrom3To8Then33() {
         int result = Counter.sum(3, 8);
-        assertEquals(33, result); // сумма чисел от 3 до 8 должна быть 33
+        assertEquals(33, result);
     }
 
     @Test
     public void whenSumFrom1To1Then1() {
         int result = Counter.sum(1, 1);
-        assertEquals(1, result); // сумма чисел от 1 до 1 должна быть 1
+        assertEquals(1, result);
     }
 
     @Test
     public void whenStartGreaterThanFinishThen0() {
         int result = Counter.sum(5, 3);
-        assertEquals(0, result); // если start больше finish, результат должен быть 0
+        assertEquals(0, result);
     }
-
-
 }
-
