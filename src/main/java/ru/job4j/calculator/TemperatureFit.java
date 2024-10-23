b43b91e55ce8e9bc105c24af1b912d83d775baae
Konstantin <89169555954@mail.ru>
2024-10-23 23:51:52 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/calculator/TemperatureFit.java
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -3,20 +3,18 @@ package ru.job4j.calculator;
 public class TemperatureFit {
 
     public static double idealTemperatureForFruits(int temperature) {
-        return (temperature < 0 || temperature > 10) ? 5.0 : temperature;
+        return temperature - 6.0;
     }
 
     public static double idealTemperatureForMeat(int temperature) {
-        return (temperature < -2 || temperature > 4) ? 1.0 : temperature;
+        return temperature - 4.0;
     }
 
     public static void main(String[] args) {
-        int temperature = 10; // Example room temperature
+        int temperature = 12; // Example room temperature
         double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
         double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
         System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
         System.out.println("Ideal temperature for meat is " + meatTemp + "°C");
     }
-}
-
 }
--- src/test/java/ru/job4j/calculator/TemperatureFitTest.java
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -6,17 +6,17 @@ import static org.assertj.core.api.Assertions.*;
 class TemperatureFitTest {
 
     @Test
-    void whenRoomTemperature10ThenFruitsIdeal4() {
-        int input = 10;
-        double expected = 4.0;
+    void whenRoomTemperature10ThenFruits6() {
+        int input = 12;
+        double expected = 6.0;
         double output = TemperatureFit.idealTemperatureForFruits(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenRoomTemperature10ThenMeatIdeal0() {
-        int input = 10;
-        double expected = 0.0;
+    void whenRoomTemperature10ThenMeat4() {
+        int input = 12;
+        double expected = 4.0;
         double output = TemperatureFit.idealTemperatureForMeat(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
