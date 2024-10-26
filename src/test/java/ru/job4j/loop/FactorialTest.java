796785073718ff626b7906950573d3a161c4d77f
Konstantin <89169555954@mail.ru>
2024-10-27 01:55:00 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FactorialTest {
+
     @Test
     void whenCalculateFactorialForFiveThenOneHundredTwenty() {
         int expected = 120;
@@ -11,8 +12,9 @@ class FactorialTest {
         int output = Factorial.calculate(number);
         assertThat(output).isEqualTo(expected);
     }
+
     @Test
-    void whenCalculateFactorialForZeroThenOne() {
+        void whenCalculateFactorialForZeroThenOne() {
         int expected = 1;
         int number = 0;
         int output = Factorial.calculate(number);
