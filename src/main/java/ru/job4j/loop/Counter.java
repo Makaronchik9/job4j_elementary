ad8017b4a568ba8e95e999e91adab42e6d674ef5
Konstantin <89169555954@mail.ru>
2024-10-26 23:56:08 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,12 +3,12 @@ package ru.job4j.loop;
 public class Counter {
 
     public static int sum(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            sum = sum + i;
+            int sum = 0;
+            for (int i = start; i <= finish; i++) {
+                sum = sum + i;
+            }
+            return sum;
         }
-        return sum;
-    }
 
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
@@ -17,4 +17,3 @@ public class Counter {
         System.out.println(sum(5, 3));
     }
 }
-
