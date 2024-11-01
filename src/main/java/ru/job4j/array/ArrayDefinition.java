d662ce9e95b70916acdee2d17b7776c3ec08cc27
Konstantin <89169555954@mail.ru>
2024-11-01 22:55:13 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
-        System.out.println("Размер массива ages: " + ages.length);
-        System.out.println("Размер массива surnames: " + surnames.length);
-        System.out.println("Размер массива prices: " + prices.length);
+        System.out.println(ages.length);
+        System.out.println(surnames.length);
+        System.out.println(prices.length);
+        String[] names = new String[5];
+        names[0] = "Костя";
+        names[1] = "Алена";
+        names[2] = "Лиза";
+        names[3] = "Теща";
+        names[4] = "Серега";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
     }
 }
