524da1044eccb993b92f3142f300222d6acb4738
Konstantin <89169555954@mail.ru>
2025-02-05 23:14:52 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/loop/Savings.java
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -2,16 +2,14 @@ package ru.job4j.loop;
 
 public class Savings {
     public static int years(double goal, double annualDeposit, double percent) {
-        double balance = 0;
         int years = 0;
+        double total = 0;
 
         do {
-            balance += annualDeposit;
-            balance += balance * (percent / 100);
+            total += total * (percent / 100);
+            total += annualDeposit;
             years++;
-        } while (balance < goal);
-
+        } while (total < goal);
         return years;
     }
 }
-
