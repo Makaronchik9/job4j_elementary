78e40a060ea26fd24c8df32d300d80893d1ede0c
Konstantin <89169555954@mail.ru>
2025-02-06 22:13:16 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,31 +1,23 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PointTest {
+public class PointTest {
 
     @Test
-    void whenPointsAnd23And23Then0() {
-        int x1 = 2, y1 = 3, x2 = 2, y2 = 3;
-        double expected = 0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    public void whenPointsAreSameThenDistanceIsZero() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(1, 2, 3);
+        double result = a.distance3d(b);
+        assertEquals(0.0, result);
     }
 
     @Test
-    void whenPointsMinus3Minus4Minus7Minus1Then2Dot5() {
-        int x1 = -3, y1 = -4, x2 = -7, y2 = -1;
-        double expected = 5.0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
-    }
-
-    @Test
-    void whenPointsAnd2M2And4And4Then5Dot83() {
-        int x1 = 2, y1 = 2, x2 = 4, y2 = 4;
-        double expected = 2.83;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    public void whenPointsAreDifferentThenCalculateDistance() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(4, 6, 8);
+        double result = a.distance3d(b);
+        assertEquals(7.071, result, 0.001);
     }
 }
