fe5c415ce6e083bb737b3b43fb5926bf90c19728
Konstantin <89169555954@mail.ru>
2025-02-12 22:11:54 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,24 +1,29 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixCheckTest {
-
     @Test
-    void monoHorizontal() {
+    void whenColumnIsFullOfXThenTrue() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'X', 'O', 'X'},
+                {'X', 'O', 'X'}
+        };
+        assertTrue(MatrixCheck.monoVertical(board, 0));
+        assertTrue(MatrixCheck.monoVertical(board, 2));
     }
 
     @Test
-    void monoVertical() {
-    }
-
-    @Test
-    void extractDiagonal() {
-    }
-
-    @Test
-    void isWin() {
+    void whenColumnHasDifferentCharsThenFalse() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'X'},
+                {'X', 'O', 'O'}
+        };
+        assertFalse(MatrixCheck.monoVertical(board, 0));
+        assertFalse(MatrixCheck.monoVertical(board, 1));
+        assertFalse(MatrixCheck.monoVertical(board, 2));
     }
 }
