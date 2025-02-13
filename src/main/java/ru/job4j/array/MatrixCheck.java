3ee09a0aeb7ce01ee4a338e77dd5eedcb73bf5f3
Konstantin <89169555954@mail.ru>
2025-02-14 00:01:18 +0300

    Фиксация изменений на github [#505270 #516742]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,9 +3,18 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static char[] extractDiagonal(char[][] board) {
         char[] result = new char[board.length];
-        for (int i = 0; i < board.length; i++) {
-            result[i] = board[i][i];
+        for (int index = 0; index < board.length; index++) {
+            result[index] = board[index][index];
         }
         return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 'X') {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixCheckTest {
+
     @Test
     public void whenDiagonalFullX() {
         char[][] input = {
@@ -13,7 +14,7 @@ public class MatrixCheckTest {
         };
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expected = {'X', 'X', 'X'};
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
@@ -25,7 +26,7 @@ public class MatrixCheckTest {
         };
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expected = {'1', '1', '1'};
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
@@ -37,7 +38,47 @@ public class MatrixCheckTest {
         };
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expected = {'X', 'Y', 'Z'};
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenMonoVerticalTrue() {
+        char[][] board = {
+                {'X', 'O', 'O'},
+                {'X', 'O', 'O'},
+                {'X', 'O', 'O'},
+        };
+        assertThat(MatrixCheck.monoVertical(board, 0)).isTrue();
+    }
+
+    @Test
+    public void whenMonoVerticalFalse() {
+        char[][] board = {
+                {'X', 'O', 'O'},
+                {'O', 'O', 'O'},
+                {'X', 'O', 'O'},
+        };
+        assertThat(MatrixCheck.monoVertical(board, 0)).isFalse();
+    }
+
+    @Test
+    public void whenMonoVerticalOnAnotherColumn() {
+        char[][] board = {
+                {'O', 'X', 'O'},
+                {'O', 'X', 'O'},
+                {'O', 'X', 'O'},
+        };
+        assertThat(MatrixCheck.monoVertical(board, 1)).isTrue();
+
+    }
+
+    @Test
+    public void whenMonoVerticalAnotherColumnFalse() {
+        char[][] board = {
+                {'O', 'X', 'O'},
+                {'O', 'O', 'O'},
+                {'O', 'X', 'O'},
+        };
+        assertThat(MatrixCheck.monoVertical(board, 1)).isFalse();
     }
 }
-
